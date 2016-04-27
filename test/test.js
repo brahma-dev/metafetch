b35681007c36458fee03d6147bf1f9ee130a3e0f
Afzaal Ameer <afzaal@afzaalace.com>
2016-04-27 15:09:33 +0200

    Added test for 302 Redirects.

--- test/test.js
+++ test/test.js
@@ -6,11 +6,22 @@ var should = require('should'),
 describe('fetchog', function() {
 	describe('#fetch()', function() {
 		it('should get a meta without error from ecoswarm.com', function(done) {
-			fetchog.fetch('http://www.ecoswarm.com/afzaalace#someanchor', function(err, meta) {
+			fetchog.fetch('http://ecoswarm.com/afzaalace#someanchor', function(err, meta) {
 				should.not.exist(err);
 				should.exist(meta);
 				should.exist(meta.uri);
 				meta.uri.host.should.equal('www.ecoswarm.com');
+				done();
+			});
+		});
+		it('should get a meta without error from nasa.gov', function(done) {
+			// www.nasa.gov adds a trailing slash
+			fetchog.fetch('http://www.nasa.gov/feature/goddard/2016/carbon-dioxide-fertilization-greening-earth', function(err, meta) {
+				should.not.exist(err);
+				should.exist(meta);
+				should.exist(meta.uri);
+				meta.uri.host.should.equal('www.nasa.gov');
+				meta.uri.path.should.equal('/feature/goddard/2016/carbon-dioxide-fertilization-greening-earth/');
 				done();
 			});
 		});
